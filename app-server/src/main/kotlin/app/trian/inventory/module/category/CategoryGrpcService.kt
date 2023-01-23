ffc11ee858c46cb00c78792d5f4b4816c599d179
azkifairuz <azkiajmal@gmail.com>
2023-01-23 21:53:40 +0700

    feat : create category

--- app-server/src/main/kotlin/app/trian/inventory/module/category/CategoryGrpcService.kt
+++ app-server/src/main/kotlin/app/trian/inventory/module/category/CategoryGrpcService.kt
@@ -5,12 +5,34 @@ import app.trian.inventory.v1.category.CategoryResponse
 import app.trian.inventory.v1.category.CreateCategoryRequest
 import app.trian.inventory.v1.category.categoryResponse
 import net.devh.boot.grpc.server.service.GrpcService
+import java.util.Date
 
 @GrpcService
 class CategoryGrpcService(
     private val categoryRepository: CategoryRepository
 ):CategoryGrpcKt.CategoryCoroutineImplBase() {
+
     override suspend fun createNewCategory(request: CreateCategoryRequest): CategoryResponse {
-        return categoryResponse {  }
+        val date = Date()
+        val category = Category()
+
+        val saveCategory = categoryRepository.save(
+            category.copy(
+                id = null,
+                categoryName = request.categoryName,
+                categoryDescription = request.categoryDescription,
+                createdAt = date,
+                updatedAt = date
+            )
+        )
+
+
+        return categoryResponse {
+            categoryId = saveCategory.id.orEmpty()
+            categoryName = saveCategory.categoryName.orEmpty()
+            categoryDescription = saveCategory.categoryDescription.orEmpty()
+            createdAt = saveCategory.createdAt.toString()
+            updatedAt = saveCategory.updatedAt.toString()
+        }
     }
 }
