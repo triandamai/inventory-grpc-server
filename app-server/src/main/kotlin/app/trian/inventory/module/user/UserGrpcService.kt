8533045e2b51fbc598a3e7dc8aea9adde8485fd1
Trian Damai <triannurizkillah@gmail.com>
2023-01-23 16:42:37 +0700

    feat: get list users

--- app-server/src/main/kotlin/app/trian/inventory/module/user/UserGrpcService.kt
+++ app-server/src/main/kotlin/app/trian/inventory/module/user/UserGrpcService.kt
@@ -1,7 +1,8 @@
 package app.trian.inventory.module.user
 
 
-import app.trian.inventory.v1.user.UserGrpcKt
+import app.trian.inventory.v1.user.*
+import kotlinx.coroutines.flow.Flow
 import net.devh.boot.grpc.server.service.GrpcService
 import org.springframework.data.domain.PageRequest
 import org.springframework.data.domain.Pageable
@@ -11,4 +12,33 @@ class UserGrpcService(
     private val userRepository: UserRepository
 ):UserGrpcKt.UserCoroutineImplBase(){
 
+    override suspend fun getListUser(request: GetListUserRequest): GetListUserResponse {
+        val dataUsers = userRepository.findAll(
+            PageRequest.of(
+                request.page.toInt(),
+                50
+            )
+        )
+
+        return getListUserResponse {
+            totalItem = dataUsers.totalElements
+            totalPage = dataUsers.totalPages.toLong()
+            data += dataUsers.content.map {
+                userResponse {
+                    userId = it.id.orEmpty()
+                    userFullName = it.userFullName
+                    userEmail = it.userEmail
+                    authProvider = it.authProvider
+                    createdAt = it.createdAt.toString()
+                    updatedAt = it.updatedAt.toString()
+
+                }
+            }
+            currentPage = dataUsers.number.toLong()
+        }
+    }
+
+    override suspend fun uploadImageUser(requests: Flow<UserImageUploadRequest>): UserImageUploadResponse {
+        return super.uploadImageUser(requests)
+    }
 }
