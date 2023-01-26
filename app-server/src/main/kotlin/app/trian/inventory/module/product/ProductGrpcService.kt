78b885330d82b65107f086e60ab3f19ad5530d17
azkifairuz <azkiajmal@gmail.com>
2023-01-26 22:21:07 +0700

    feat : product

--- app-server/src/main/kotlin/app/trian/inventory/module/product/ProductGrpcService.kt
+++ app-server/src/main/kotlin/app/trian/inventory/module/product/ProductGrpcService.kt
@@ -1,17 +1,19 @@
 package app.trian.inventory.module.product
 
 import app.trian.inventory.v1.GetPagingRequest
-import app.trian.inventory.v1.product.GetListProductResponse
-import app.trian.inventory.v1.product.ProductGrpcKt
+import app.trian.inventory.v1.product.*
 import net.devh.boot.grpc.server.service.GrpcService
 
 @GrpcService
 class ProductGrpcService(
-    private val productRepository: ProductRepository
+    private val productService: ProductService
 ) :ProductGrpcKt.ProductCoroutineImplBase(){
 
-    override suspend fun getListProduct(request: GetPagingRequest): GetListProductResponse {
-        return super.getListProduct(request)
-    }
+    override suspend fun getListProduct(request: GetPagingRequest): GetListProductResponse = productService.getListProduct(request)
 
+    override suspend fun createNewProduct(request: CreateProductRequest): ProductResponse  = productService.createNewCategory(request)
+
+    override suspend fun updateProduct(request: UpdateProductRequest): ProductResponse  = productService.updateCategory(request)
+
+    override suspend fun deleteProduct(request: DeleteProductRequest): ProductResponse= productService.deleteProduct(request)
 }
